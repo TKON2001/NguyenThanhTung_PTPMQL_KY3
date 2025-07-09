0623d36c828312e9b401cb9a6b9f49c7a9eec067
NguyenThanhTung_1921050664_KiemTra_2425 <tkon33322@gmail.com>
2025-07-09 18:08:37 +0700

    BTH_0907_SinhIdTuDong

---
+++ DemoMvc/Migrations/20250709100327_AddSalaryAndAgeToEmployee.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DemoMvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSalaryAndAgeToEmployee : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Salary",
+                table: "Persons",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Salary",
+                table: "Persons");
+        }
+    }
+}
--- DemoMvc/obj/Debug/net9.0/DemoMvc.AssemblyInfo.cs
+++ DemoMvc/obj/Debug/net9.0/DemoMvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dc9a95eaf6bf755ae94be652f9ddfb1a9cb2c0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4efc25da7443749345774718355b4fbbfc81948")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
